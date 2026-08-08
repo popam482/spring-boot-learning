5f8a13a431f987293eb59ac4a3c5973dfbb2f4e3
popam482 <popamaria482@gmail.com>
2026-08-08 21:05:02 +0300

    added post functions in the frontend with auto refresh after adding tasks

--- frontend/task-manager-frontend/src/App.jsx
+++ frontend/task-manager-frontend/src/App.jsx
@@ -1,14 +1,44 @@
 import {useState, useEffect} from 'react'
 import './App.css';
 
+
 function App() {
     const [tasks, taskSet] = useState([])
+    const [newTitle, setNewTitle] = useState('')
+    const [newDescription, setNewDescription] = useState('')
+    const [newPriority, setNewPriority] = useState('LOW')
+    const [newCompletion, setNewCompletion] = useState(false)
+
+    const [isOpen, setOpen] = useState(false);
 
     useEffect(() =>{
         fetch('http://localhost:8080/tasks')
             .then(response => response.json())
             .then(data => taskSet(data))
     }, [])
+
+
+    function addTask(){
+        fetch('http://localhost:8080/tasks', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title: newTitle,
+                description: newDescription,
+                priority: newPriority
+            })
+        })
+            .then(response => response.json())
+            .then(data => {
+                taskSet([...tasks, data]);
+                setNewTitle('');
+                setNewDescription('');
+                setNewPriority('');
+                setOpen(false);
+            });
+    }
 
     return (
         <div className="container">
@@ -31,6 +61,31 @@ function App() {
                     </li>
                 ))}
             </ul>
+            <button onClick={() => setOpen(true)}>Add task</button>
+            {isOpen && (
+                <div className="popupContent">
+                    <h2>Add a new task</h2>
+                    <input
+                        type="text"
+                        value={newTitle}
+                        onChange={(e) => setNewTitle(e.target.value)}
+                        placeholder="Task title"
+                    />
+                    <input
+                        type="text"
+                        value={newDescription}
+                        onChange={(e) => setNewDescription(e.target.value)}
+                        placeholder="Task description"
+                    />
+                    <select value={newPriority} onChange={(e) => setNewPriority(e.target.value)}>
+                        <option value="LOW">LOW</option>
+                        <option value="MEDIUM">MEDIUM</option>
+                        <option value="HIGH">HIGH</option>
+                    </select>
+                    <button onClick={addTask}>Add</button>
+                    <button onClick={() => setOpen(false)}>Close</button>
+                </div>
+            )}
         </div>
     )
 }
