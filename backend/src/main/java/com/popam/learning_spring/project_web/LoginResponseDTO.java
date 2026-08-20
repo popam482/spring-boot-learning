e93d11dc37bfc815651a4f803454112cb1f4390d
popam482 <popamaria482@gmail.com>
2026-08-20 16:59:22 +0300

    spring security: added login response dto

---
+++ backend/src/main/java/com/popam/learning_spring/project_web/LoginResponseDTO.java
@@ -0,0 +1,32 @@
+package com.popam.learning_spring.project_web;
+
+public class LoginResponseDTO {
+
+    private String token;
+    private UserResponseDTO user;
+
+    public LoginResponseDTO() {
+
+    }
+
+    public LoginResponseDTO(String token, UserResponseDTO user) {
+        this.token = token;
+        this.user = user;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public UserResponseDTO getUser() {
+        return user;
+    }
+
+    public void setUser(UserResponseDTO user) {
+        this.user = user;
+    }
+}
