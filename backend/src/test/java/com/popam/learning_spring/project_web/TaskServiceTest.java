4552d28c9c3186556161eafac3a08136120b1a09
popam482 <popamaria482@gmail.com>
2026-08-26 19:19:59 +0300

    spring testing mockito - added task tests to check wether it exists or not

--- backend/src/test/java/com/popam/learning_spring/project_web/TaskServiceTest.java
+++ backend/src/test/java/com/popam/learning_spring/project_web/TaskServiceTest.java
@@ -9,8 +9,8 @@ import org.springframework.security.access.AccessDeniedException;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -68,5 +68,49 @@ public class TaskServiceTest {
                 AccessDeniedException.class,
                 () -> taskService.getTaskById(1, "testUser")
         );
+    }
+
+    @Test
+    void getTaskById_returnsTask() {
+        // Arrange
+        Task task = new Task();
+        task.setId(1);
+        task.setTitle("Test task");
+        task.setDescription("Test description");
+
+        User user = new User();
+        user.setUsername("testUser");
+        task.setUser(user);
+
+        when(taskRepository.findById(1))
+                .thenReturn(Optional.of(task));
+
+        // Act
+        TaskResponseDTO result = taskService.getTaskById(1, "testUser");
+
+        // Assert
+        assertNotNull(result);
+        assertEquals(1, result.getId());
+        assertEquals("Test task", result.getTitle());
+        assertEquals("Test description", result.getDescription());
+        assertEquals("testUser", result.getUsername());
+
+        verify(taskRepository).findById(1);
+    }
+
+
+    @Test
+    void getTaskById_throwsTaskNotFound() {
+        //Arrange
+        when(taskRepository.findById(1))
+                .thenReturn(Optional.empty());
+
+        // Assert
+        assertThrows(
+                TaskNotFound.class,
+                () -> taskService.getTaskById(1, "testUser")
+        );
+
+        verify(taskRepository).findById(1);
     }
 }
